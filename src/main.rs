a679cd753630712da8b22425b12cd269853f00b1
Rutger van der Huizen <rutger.vanderhuizen@essent.nl>
2024-12-02 11:41:48 +0100

    start to reading input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+
+fn get_input(path: &str) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for line in fs::read_to_string(path).unwrap().lines() {
+        result.push(line.to_string())
+    }
+    result
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let arg = args.last();
+    match arg {
+        Some(arg) => {
+            let input = get_input(&format!("./data/input-{}.txt", arg));
+            println!("{:?}", input);
+        },
+        None => panic!("No argument provided"), 
+    }
 }
