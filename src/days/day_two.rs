e3dbf704877dfcd54f7c31bb81500378cc102bd5
Rutger van der Huizen <rutger.vanderhuizen@essent.nl>
2024-12-03 11:46:56 +0100

    refactor & day 2 part 2

--- src/days/day_two.rs
+++ src/days/day_two.rs
@@ -2,39 +2,69 @@ use crate::structs::problem::Problem;
 
 pub struct DayTwo {}
 
+fn is_safe(report: &[i32]) -> bool {
+    let mut increasing = None;
+    for i in 0..report.len() - 1 {
+        let current = report[i];
+        let next = report[i + 1];
+        let diff = (current - next).abs();
+
+        if diff < 1 || diff > 3 {
+            return false;
+        }
+
+        match increasing {
+            None => increasing = Some(current < next),
+            Some(true) if current >= next => return false,
+            Some(false) if current <= next => return false,
+            _ => {}
+        }
+    }
+    true
+}
+
 impl Problem for DayTwo {
-    /**
-    *  The levels are either all increasing or all decreasing.
-       Any two adjacent levels differ by at least one and at most three.
-    */
     fn part_one(&self, input: Vec<String>) -> String {
         let mut result = 0;
         for line in input {
-            let split_line: Vec<i32> = line
+            let levels: Vec<i32> = line
                 .split_whitespace()
                 .map(|s| s.parse::<i32>().unwrap())
                 .collect();
-            let mut increasing = false;
-            for i in 0..split_line.len() {
-                if i == split_line.len() - 1 {
-                    result += 1;
-                    break;
-                }
-                let current = split_line[i];
-                let next = split_line[i + 1];
-                let diff = i32::abs(current - next);
-                if i == 0 {
-                    increasing = current < next;
-                }
-                if diff < 1 || diff > 3 || if !increasing { current < next } else { current > next } {
-                    break;
-                }
+            if is_safe(&levels) {
+                result += 1;
             }
         }
         result.to_string()
     }
 
     fn part_two(&self, input: Vec<String>) -> String {
-        todo!()
+        let mut result = 0;
+
+        for line in input {
+            let levels: Vec<i32> = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect();
+
+            if is_safe(&levels) {
+                result += 1;
+            } else {
+                let mut dampener_safe = false;
+                for i in 0..levels.len() {
+                    let mut modified_levels = levels.clone();
+                    modified_levels.remove(i);
+                    if is_safe(&modified_levels) {
+                        dampener_safe = true;
+                        break;
+                    }
+                }
+
+                if dampener_safe {
+                    result += 1;
+                }
+            }
+        }
+        result.to_string()
     }
 }
