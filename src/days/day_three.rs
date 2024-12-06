8695cc830c76d62c22bf5a06a81043aa9927d298
rut13 <58955931+rut13@users.noreply.github.com>
2024-12-07 00:22:37 +0100

    finish 3 2

--- src/days/day_three.rs
+++ src/days/day_three.rs
@@ -2,6 +2,14 @@ use crate::structs::problem::Problem;
 use regex::Regex;
 pub struct DayThree {}
 
+fn total_sum(mults: &Vec<(u32, u32)>) -> u32 {
+    let mut sum = 0;
+    for (first, second) in mults {
+        sum += first * second;
+    }
+    sum
+}
+
 impl Problem for DayThree {
     fn part_one(&self, input: Vec<String>) -> String {
         let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
@@ -12,15 +20,41 @@ impl Problem for DayThree {
             Some((first, second))
         }).collect();
 
-        let mut sum = 0;
-        for (first, second) in mults {
-            sum += first * second;
-        }
-
-        sum.to_string()
+        total_sum(&mults).to_string()
     }
 
-    fn part_two(&self, _input: Vec<String>) -> String {
-        todo!()
+    fn part_two(&self, input: Vec<String>) -> String {
+        let re = Regex::new(r"mul\((\d+),(\d+)\)|do(?:n't)?\(\)").unwrap();
+        let merged = input.clone().join("");
+        let mut allow = None;
+        let mults: Vec<(u32, u32)> = re.captures_iter(&merged).filter_map(|caps| {
+            if let Some(do_match) = caps.get(0) {
+                let first = do_match.as_str();
+                if first.starts_with("do(") {
+                    allow = Some(true);
+                    return None;
+                } else if first.starts_with("don't(") {
+                    allow = Some(false);
+                    return None;
+                }
+            }
+
+            if let (Some(first_num), Some(second_num)) = (caps.get(1), caps.get(2)) {
+                match allow {
+                    Some(true) | None => {
+                        let first = first_num.as_str().parse::<u32>().ok()?;
+                        let second = second_num.as_str().parse::<u32>().ok()?;
+                        return Some((first, second));
+                    }
+                    _ => return None,
+                }
+            }
+
+            None
+        }).collect();
+
+
+
+        total_sum(&mults).to_string()
     }
 }
