7a2defdccda93d1a2c4f00ffebc83410833d4409
rut13 <58955931+rut13@users.noreply.github.com>
2024-12-06 23:46:22 +0100

    finish 3 1

--- src/days/day_three.rs
+++ src/days/day_three.rs
@@ -4,20 +4,20 @@ pub struct DayThree {}
 
 impl Problem for DayThree {
     fn part_one(&self, input: Vec<String>) -> String {
-        let re = Regex::new(r"/mul\((\d+),(\d+)\)/").unwrap();
+        let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
         let merged = input.clone().join("");
-        let mults = re.captures_iter(&merged);
-        // .map(|caps| {
-        //     // let (abc, [first, second, third]) = caps.extract();
-        //     let first = caps.get(1).map_or("", |m| m.as_str()).to_string();
-        //     let second = caps.get(2).map_or("", |m| m.as_str()).to_string();
-        //     println!("first{:?}", first);
-        //     println!("second{:?}", second);
-        //     (first, second)
-        // }).collect();
+        let mults: Vec<(u32, u32)> = re.captures_iter(&merged).filter_map(|caps| {
+            let first = caps.get(1)?.as_str().parse::<u32>().ok()?;
+            let second = caps.get(2)?.as_str().parse::<u32>().ok()?;
+            Some((first, second))
+        }).collect();
 
-        println!("{:?}", mults);
-        "".to_string()
+        let mut sum = 0;
+        for (first, second) in mults {
+            sum += first * second;
+        }
+
+        sum.to_string()
     }
 
     fn part_two(&self, _input: Vec<String>) -> String {
