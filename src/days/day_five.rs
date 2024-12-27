7cfa9decda20b297d1b21c54cc082b856b3b7079
Rutger van der Huizen <rutger.vanderhuizen@essent.nl>
2024-12-27 11:00:08 +0100

    boilerplate for day 5

---
+++ src/days/day_five.rs
@@ -0,0 +1,14 @@
+use crate::structs::problem::Problem;
+
+
+pub struct DayFive {}
+
+impl Problem for DayFive {
+    fn part_one(&self, input: String) -> String {
+        todo!()
+    }
+
+    fn part_two(&self, _: String) -> String {
+        todo!()
+    }
+}
